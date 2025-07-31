7552a1a5fa587aee9a3d92fd340b4c885ee9e087
Matinhuu <enzodematos19@gmail.com>
2025-07-30 21:10:38 -0300

    First commit/subindo projeto

---
+++ app.js
@@ -0,0 +1,54 @@
+import express from 'express'
+const api = express();
+
+
+api.get('/calculadora/somar/:num1/:num2', (req, resp) => {
+  let n1 = Number(req.params.num1);
+  let n2 = Number(req.params.num2);
+
+  let soma = n1 + n2;
+
+
+  resp.send({
+    resultado: soma
+  });
+})
+
+api.get('/calculadora/subtrair/:num1/:num2', (req, resp) => {
+  let n1 = Number(req.params.num1);
+  let n2 = Number(req.params.num2);
+
+  let subtracao = n1 - n2;
+
+
+  resp.send({
+    resultado: subtracao
+  });
+})
+
+api.get('/calculadora/divisao/:num1/:num2', (req, resp) => {
+  let n1 = Number(req.params.num1);
+  let n2 = Number(req.params.num2);
+
+  let divisao = n1 / n2;
+
+
+  resp.send({
+    resultado: divisao
+  });
+})
+
+api.get('/calculadora/mult/:num1/:num2', (req, resp) => {
+  let n1 = Number(req.params.num1);
+  let n2 = Number(req.params.num2);
+
+  let mult = n1 * n2;
+
+
+  resp.send({
+    resultado: mult
+  });
+})
+
+
+api.listen(5010, () => console.log('... funcionou :) '));
