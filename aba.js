d3915fc8f4f1861db38891239be69a5d20ebbd5c
Matinhuu <enzodematos19@gmail.com>
2025-07-30 22:24:45 -0300

    Deleting temporary/test files (aba.js)

--- aba.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const nome = express();
-
-nome.get('/mensagem',(req,resp) => {
-    resp.send(`helloworld`)
-})
-nome.listen(5001,
-    () => console.log(`--> QUERO VOLTAR COM MINHA EX <-- `)); 
